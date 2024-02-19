69ff0ba069b2866ae62abebcd7436d8be8a8b5e1
anita <anita8004@gmail.com>
2024-02-19 16:13:52 +0800

    update nav

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -7,8 +7,8 @@
       </div>
     </div>
     <nav class="logo_menu">
-      <router-link to="/"><h1>JSY</h1></router-link>
-      <ul class="menu">
+      <h1><router-link to="/">JSY</router-link></h1>
+      <ul :class="['menu', { show: menuIsShow }]">
         <li>
           <router-link to="/home">HOME & Living</router-link>
         </li>
@@ -26,23 +26,30 @@
         </li>
       </ul>
     </nav>
-    <ul class="member">
-      <li>
-        <a href="#"><font-awesome-icon icon="fa-solid fa-user" /></a>
-      </li>
-      <li class="cart_num">
-        <a href="#">Cart(0)</a>
-      </li>
-    </ul>
-    <div class="phone_menu">
-      <font-awesome-icon icon="fa-solid fa-bars" />
+    <div class="member">
+      <a href="#"><font-awesome-icon icon="fa-solid fa-user" /></a>
+      <a href="#">Cart(0)</a>
+      <button :class="['phone_menu', { active: menuIsShow }]" @click="toggleMenu">
+        <font-awesome-icon :icon="`fa-solid ${menuIsShow ? 'fa-xmark' : 'fa-bars'}`" />
+      </button>
     </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const menuIsShow = ref(false);
+
+const toggleMenu = () => {
+  menuIsShow.value = !menuIsShow.value;
+}
+
+
+</script>
 
 <style lang="scss" scoped>
+@import "../../assets/scss/media";
+
 * {
   box-sizing: border-box;
   color: #000;
@@ -50,9 +57,12 @@
 header {
   width: 100%;
   padding: 0 20px;
-  // border: 5px solid gold;
   display: grid;
   grid-template-columns: 1fr 2fr 1fr;
+  @include pad {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
 }
 
 .search {
@@ -72,6 +82,9 @@ header {
       border-bottom: 2px solid #000;
     }
   }
+  @include pad {
+    display: none;
+  }
 }
 
 .logo_menu {
@@ -79,80 +92,61 @@ header {
   display: grid;
   grid-template-rows: repeat(2, 1fr);
   h1 {
-    border: 1px solid greenyellow;
+    // border: 1px solid greenyellow;
     text-align: center;
     padding: 30px 0;
     box-sizing: border-box;
+  }
+  @include pad {
+    grid-template-rows: unset;
   }
 }
 
 .menu {
   text-align: center;
   display: flex;
-  padding: 20px 0;
-  border: 5px solid gray;
   gap: 20px;
-  padding-left: 20px;
   box-sizing: border-box;
   justify-content: space-evenly;
   align-items: center;
+  @include pad {
+    flex-direction: column;
+    justify-content: center;
+    gap: 5vh;
+    font-size: 2rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 100;
+    background-color: #fff;
+    &:not(.show) {
+      display: none;
+    }
+  }
 }
 
 .member {
   display: flex;
   align-items: center;
-  justify-content: right;
-  border: 10px solid red;
-
-  li {
-    margin-right: 30px;
-    border: 10px solid red;
-  }
+  justify-content: center;
+  gap: .5rem;
 }
 
 .phone_menu{
   display: none;
-}
-
-
-
-@media (max-width: 768px) {
-  header {
-    display: grid;
-    grid-template-columns: 2fr 1fr 1fr;
-    height: 90px;
-    padding: 0 0;
-    .logo_menu {
-      .menu {
-        display: none;
-      }
-    }
-    .search {
-      display: none;
-    }
-    .member {
-      // display: flex;
-      border: none;
-      height: 90px;
-      // justify-content: center;
-      // align-items: center;
-      li {
-        border: none;
-        list-style: none;
-        a {
-          text-decoration: none;
-        }
-      }
-      li:first-child {
-        display: none;
-      }
-    }
-    .phone_menu{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 90px;  //似乎都要被下固定寬???
+  @include pad {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: 101;
+    &.active {
+      position: fixed;
+      right: 20px;
     }
   }
 }
+
 </style>
